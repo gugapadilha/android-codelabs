50a62518c201ead83a833cc9262fb83c1e83de94
gugapadilha <guga.santospadilha@rede.ulbra.br>
2021-10-07 17:31:33 -0300

    firstProjectAndroid

---
+++ PrimeiroApp/app/src/main/java/com/example/primeiroapp/MainActivity.kt
@@ -0,0 +1,33 @@
+package com.example.primeiroapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    //caso precise instanciar variaveis
+
+    lateinit var aText: TextView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main) //todo codigo vai aqui em baixo, pois o XML precisa estar aberto
+
+        botao.setOnClickListener(object : View.OnClickListener{
+            override fun onClick(v: View?) {
+
+                var textoDigitado = nome.text.toString()
+
+                Toast.makeText(applicationContext, textoDigitado, Toast.LENGTH_LONG).show()
+            }
+
+        })
+
+    }
+}
