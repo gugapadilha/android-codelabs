0d22331adbdb0ee844fbc4dfe0a0fc18bcca2533
gugapadilha <guga.santospadilha@rede.ulbra.br>
2022-02-23 09:54:51 -0300

    talkBackImpl

--- atv-unit-2/atvTipTime/app/src/main/java/com/example/tiptime/MainActivity.kt
+++ atv-unit-2/atvTipTime/app/src/main/java/com/example/tiptime/MainActivity.kt
@@ -1,10 +1,14 @@
 package com.example.tiptime
 
 import android.annotation.SuppressLint
+import android.content.Context
 import android.icu.text.NumberFormat
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.KeyEvent
+import android.view.View
+import android.view.inputmethod.InputMethodManager
 import androidx.annotation.RequiresApi
 import com.example.tiptime.databinding.ActivityMainBinding
 
@@ -20,6 +24,8 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         binding.calculateButton.setOnClickListener { calculateTip() }
+
+        binding.costOfServiceEditText.setOnKeyListener{view, keyCode, _ -> handleKeyEvent(view, keyCode)}
     }
 
     @SuppressLint("StringFormatInvalid")
@@ -46,4 +52,17 @@ class MainActivity : AppCompatActivity() {
         val formattedTip = NumberFormat.getCurrencyInstance().format(tip)
         binding.tipResult.text = getString(R.string.tip_amount, formattedTip)
     }
+
+    private fun handleKeyEvent(view: View, keyCode: Int): Boolean {
+        if (keyCode == KeyEvent.KEYCODE_ENTER) {
+            // Hide the keyboard
+            val inputMethodManager =
+                getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
+            return true
+        }
+        return false
+    }
+
+
 }
