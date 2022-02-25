914f1d67342ae157bf515f99feae047eeb4b8507
gugapadilha <guga.santospadilha@rede.ulbra.br>
2022-02-25 13:01:20 -0300

    testingCollors&DarkMode

--- atv-unit-2/TestingColors/app/src/main/java/com/example/testingcolors/MainActivity.kt
+++ atv-unit-2/TestingColors/app/src/main/java/com/example/testingcolors/MainActivity.kt
@@ -7,5 +7,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //nothing to click to show something. just the layout screen
     }
 }
