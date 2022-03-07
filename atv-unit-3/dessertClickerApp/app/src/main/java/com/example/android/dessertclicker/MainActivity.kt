b2badd0feba45be90ea258e3ffb57fd01691ca36
gugapadilha <guga.santospadilha@rede.ulbra.br>
2022-03-07 16:04:37 -0300

    dessertApp

---
+++ atv-unit-3/dessertClickerApp/app/src/main/java/com/example/android/dessertclicker/MainActivity.kt
@@ -0,0 +1,178 @@
+package com.example.android.dessertclicker
+
+import android.content.ActivityNotFoundException
+import android.os.Bundle
+import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ShareCompat
+import androidx.databinding.DataBindingUtil
+import com.example.android.dessertclicker.databinding.ActivityMainBinding
+
+// onSaveInstanceState Bundle Keys
+const val KEY_REVENUE = "revenue_key"
+const val KEY_DESSERT_SOLD = "dessert_sold_key"
+
+// tag for logging
+const val TAG = "MainActivity"
+
+class MainActivity : AppCompatActivity() {
+
+    private var revenue = 0
+    private var dessertsSold = 0
+
+    // Contains all the views
+    private lateinit var binding: ActivityMainBinding
+
+    data class Dessert(val imageId: Int, val price: Int, val startProductionAmount: Int)
+
+    private val allDesserts = listOf(
+            Dessert(R.drawable.cupcake, 5, 0),
+            Dessert(R.drawable.donut, 10, 5),
+            Dessert(R.drawable.eclair, 15, 20),
+            Dessert(R.drawable.froyo, 30, 50),
+            Dessert(R.drawable.gingerbread, 50, 100),
+            Dessert(R.drawable.honeycomb, 100, 200),
+            Dessert(R.drawable.icecreamsandwich, 500, 500),
+            Dessert(R.drawable.jellybean, 1000, 1000),
+            Dessert(R.drawable.kitkat, 2000, 2000),
+            Dessert(R.drawable.lollipop, 3000, 4000),
+            Dessert(R.drawable.marshmallow, 4000, 8000),
+            Dessert(R.drawable.nougat, 5000, 16000),
+            Dessert(R.drawable.oreo, 6000, 20000)
+    )
+    private var currentDessert = allDesserts[0]
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        Log.d(TAG, "onCreated Called")
+
+        // Use Data Binding to get reference to the views
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        binding.dessertButton.setOnClickListener {
+            onDessertClicked()
+        }
+
+        // If there is a savedInstanceState bundle, then you're "restarting" the activity
+        // If there isn't a bundle, then it's a "fresh" start
+        if (savedInstanceState != null) {
+            revenue = savedInstanceState.getInt(KEY_REVENUE, 0)
+            dessertsSold = savedInstanceState.getInt(KEY_DESSERT_SOLD, 0)
+            // Show the correct dessert
+            showCurrentDessert()
+        }
+
+        // Set the TextViews to the right values
+        binding.revenue = revenue
+        binding.amountSold = dessertsSold
+
+        // Make sure the correct dessert is showing
+        binding.dessertButton.setImageResource(currentDessert.imageId)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG, "onStart Called")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG, "onResume Called")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG, "onPause Called")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG, "onStop Called")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, "onDestroy Called")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.d(TAG, "onRestart Called")
+    }
+
+    /**
+     * Updates the score when the dessert is clicked. Possibly shows a new dessert.
+     */
+    private fun onDessertClicked() {
+
+        // Update the score
+        revenue += currentDessert.price
+        dessertsSold++
+
+        binding.revenue = revenue
+        binding.amountSold = dessertsSold
+
+        // Show the next dessert
+        showCurrentDessert()
+    }
+
+    /**
+     * Determine which dessert to show.
+     */
+    private fun showCurrentDessert() {
+        var newDessert = allDesserts[0]
+        for (dessert in allDesserts) {
+            if (dessertsSold >= dessert.startProductionAmount) {
+                newDessert = dessert
+            }
+            // The list of desserts is sorted by startProductionAmount. As you sell more desserts,
+            // you'll start producing more expensive desserts as determined by startProductionAmount
+            // We know to break as soon as we see a dessert who's "startProductionAmount" is greater
+            // than the amount sold.
+            else break
+        }
+
+        // If the new dessert is actually different than the current dessert, update the image
+        if (newDessert != currentDessert) {
+            currentDessert = newDessert
+            binding.dessertButton.setImageResource(newDessert.imageId)
+        }
+    }
+
+    /*** Called when the user navigates away from the app but might come back*/
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        Log.d(TAG, "onSaveInstanceState Called")
+        outState.putInt(KEY_REVENUE, revenue)
+        outState.putInt(KEY_DESSERT_SOLD, dessertsSold)
+    }
+
+    /*** Menu methods*/
+    private fun onShare() {
+        val shareIntent = ShareCompat.IntentBuilder.from(this)
+                .setText(getString(R.string.share_text, dessertsSold, revenue))
+                .setType("text/plain")
+                .intent
+        try {
+            startActivity(shareIntent)
+        } catch (ex: ActivityNotFoundException) {
+            Toast.makeText(this, getString(R.string.sharing_not_available),
+                    Toast.LENGTH_LONG).show()
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.shareMenuButton -> onShare()
+        }
+        return super.onOptionsItemSelected(item)
+    }
+}
