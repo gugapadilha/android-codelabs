5e283a57a268117aab56a25d0b9facb1c5ba4ce0
gugapadilha <guga.santospadilha@rede.ulbra.br>
2022-03-08 16:48:22 -0300

    newAppImplementations

---
+++ atv-unit-3/unscrambleApp/app/src/main/java/com/example/android/unscramble/ui/game/GameViewModel.kt
@@ -0,0 +1,12 @@
+package com.example.android.unscramble.ui.game
+
+import androidx.lifecycle.ViewModel
+
+class GameViewModel: ViewModel() {
+
+    private val viewModel = GameViewModel()
+
+    private var score = 0
+    private var currentWordCount = 0
+    private var currentScrambledWord = "test"
+}
