58c511d495e70633810ec762420bc13f2bc22483
gugapadilha <guga.santospadilha@rede.ulbra.br>
2022-03-24 12:03:13 -0300

    fixingErrors

---
+++ atv-unit-5/forageApp/app/src/main/java/com/example/forage/data/ForageableDao.kt
@@ -0,0 +1,48 @@
+/*
+ * Copyright (C) 2021 The Android Open Source Project.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.example.forage.data
+
+import androidx.room.*
+import com.example.forage.model.Forageable
+import kotlinx.coroutines.flow.Flow
+
+/**
+ * Data Access Object for database interaction.
+ */
+@Dao
+interface ForageableDao {
+
+    // TODO: implement a method to retrieve all Forageables from the database
+    @Query("SELECT * from Forageable ORDER BY name ASC")
+    fun getForageables(): Flow<List<Forageable>>
+
+    // TODO: implement a method to retrieve a Forageable from the database by id
+    @Query("SELECT * from Forageable WHERE id = :id")
+    fun getForageable(id: Long): Flow<Forageable>
+
+    // TODO: implement a method to insert a Forageable into the database
+    //  (use OnConflictStrategy.REPLACE)
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insert(foregeable: Forageable)
+
+    // TODO: implement a method to update a Forageable that is already in the database
+    @Update
+    suspend fun update(forageable: Forageable)
+
+    // TODO: implement a method to delete a Forageable from the database.
+    @Update
+    suspend fun delete(forageable: Forageable)
+}
